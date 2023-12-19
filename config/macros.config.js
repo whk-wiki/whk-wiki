58e52720cd1c4f416cb768145ccde69cba2de19c
dbxxx <3364835621@qq.com>
2023-12-19 11:14:23 +0800

    feat(katex): add macros for \nimplies, \nimpliedby and \niff

---
+++ config/macros.config.js
@@ -0,0 +1,5 @@
+export default {
+	"\\nimplies": "\\mathrel{\\mathclap{\\qquad\\backslash}{\\implies}}",
+	"\\nimpliedby": "\\mathrel{\\mathclap{\\qquad\\enspace\\backslash}{\\impliedby}}",
+	"\\niff": "\\mathrel{\\mathclap{\\qquad\\enspace\\backslash}{\\iff}}"
+}
